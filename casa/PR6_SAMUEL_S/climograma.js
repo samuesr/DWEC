947bc1c7f788d68d47109514b41436404328963f
Samuel Sanchez <samuel.sanram@educa.jcyl.es>
2023-11-08 12:24:07 +0100

    CASA

---
+++ casa/PR6_SAMUEL_S/climograma.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", finiciar, false);
+function finiciar() {
+    let precipitaciones = new Array();
+    let temperaturas = new Array();
+    let meses = new Array('ene', 'feb', 'mar', 'abr', 'may', 'jun', 'jul', 'ago', 'sep', 'oct', 'nov', 'dic');
+
+    for (let i = 0; i < meses.length; i++) {
+        let temp;
+        let tprev;
+        do {
+            temp = window.prompt("Introduce temperatura para " + meses[i]);
+            if (i == 0) { tprev = temp } else { tprev = temperaturas[i - 1] }
+        } while (isNaN(temp) || parseInt(temp) < -10 || parseInt(temp) > 40 || parseInt(temp) != temp || Math.abs(temp - temperaturas[i - 1]) > 8);
+        temperaturas[i] = temp;
+        console.log(temperaturas[i]);
+    }
+    let precipitacion;
+    for (let i = 0; i < meses.length; i++) {
+        do {
+            precipitacion = window.prompt("Intoduce precipitacion para " + meses[i]);
+        } while (isNaN(precipitacion) || parseInt(precipitacion) < 0 || parseInt(precipitacion) > 50 || parseInt(precipitacion) != precipitacion);
+        precipitaciones.push(precipitacion);
+    }
+    function pintar(unarray, mes, titulo) {
+        let contenido = " <h1>" + titulo + "</h1><p>";
+        console.log(contenido);
+
+        for (let i = 0; i < unarray.length; i++) {
+
+            contenido += mes[i] + "\t || \t";
+            console.log(contenido);
+            for (let index = 0; index < Math.abs(unarray[i]); index++) {
+                contenido += "=";
+            }
+            contenido += unarray[i] + "</p>";
+            console.log(contenido);
+        }
+        return contenido;
+    }
+    document.getElementById('temperatura').innerHTML = pintar(temperaturas, meses, "TEMPERATURAS");
+    document.getElementById('precipitacion').innerHTML = pintar(precipitaciones, meses, "PRECIPITACIONES");
+
+}
---
+++ libreria.js
@@ -0,0 +1,27 @@
+
+       
+        function Libro(nombre,autor,tema) {
+            this.nombre=nombre;
+            this.autor=autor;
+            this.tema=tema;
+        }
+        let tamaño;
+        let biblioteca= new Array ();
+        do {
+            tamaño=window.prompt("Introduce tamaño de la libreria",0);
+        } while (isNaN(parseInt(tamaño))|| parseInt(tamaño)<0 || parseInt(tamaño)!=tamaño);
+        for (let index = 0; index < tamaño; index++) {
+                let nombre=window.prompt("Introduce el nombre del libro");
+                let autor=window.prompt("Introduce el autor del libro");
+                let tema=window.prompt("Introduce el tema del libro");
+
+                let libro= new Libro(nombre, autor, tema);
+
+                window.alert(biblioteca.push(libro));
+        }
+        let mitabla='<table><tr><td>Nombre</td><td>Autor</td><td>Tema</td></tr>';
+        biblioteca.forEach(element => {
+            mitabla+="<tr><td>"+element.nombre+"</td><td>"+element.autor+"</td><td>"+element.tema+"</td></tr>";  
+        });
+        mitabla+="</table>";
+        document.getElementById('tabla').innerHTML=mitabla;

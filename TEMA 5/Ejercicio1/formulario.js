74efbe849c746d92ee263a308fa2a19b46ff2258
Samuel <samuel.sanram@educa.jcyl.es>
2023-11-15 19:34:24 +0100

    ejerciciod

---
+++ TEMA 5/Ejercicio1/formulario.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded',iniciar,false);
+function iniciar(){
+    document.getElementById('opcion2').addEventListener('click',cambio);
+    document.getElementById('opcion1').addEventListener('click',cambio2);
+}
+function cambio(){
+    document.getElementById('oculto').style.display='inline';
+}
+function cambio2(){
+    document.getElementById('oculto').style.display='none';
+}
+document.addEventListener('DOMContentLoaded',iniciar,false);
+function iniciar(){
+    document.getElementById('opcion2').addEventListener('click',cambio);
+    document.getElementById('opcion1').addEventListener('click',cambio2);
+}
+function cambio(){
+    document.getElementById('oculto').style.display='inline';
+}
+function cambio2(){
+    document.getElementById('oculto').style.display='none';
+}

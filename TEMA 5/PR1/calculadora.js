ad93ce4df2061fd2a925efc6ee179627ef094b87
Samuel Sanchez <samuel.sanram@educa.jcyl.es>
2023-11-16 10:02:58 +0100

    calculadora

--- TEMA 5/PR1/calculadora.js
+++ TEMA 5/PR1/calculadora.js
@@ -1,43 +1,31 @@
 document.addEventListener('DOMContentLoaded', finiciar, false);
-
-
 function getValue() {
-    //console.log(this.value);
     let num = this.value;
     let contenido = document.getElementById('resultado').value;
-    //console.log(contenido + this.value);
     switch (num) {
         case 'EXE':
             if (contenido == "" || isNaN(contenido.charAt(contenido.length - 1))) {
                 document.getElementById('resultado').value = contenido;
             } else {
-                document.getElementById('resultado').value = eval(contenido);
+             if(contenido.includes("MOD")){contenido=contenido.replace("MOD","%")}
+             if(contenido.includes("^")){
+                console.log('entré');
+                contenido=contenido.replace("^","**")}
+             console.log(contenido);
+             document.getElementById('resultado').value=eval(contenido);
             }
-            console.log(contenido.charAt(contenido.length - 1));
             break;
         case 'BORRAR':
             document.getElementById('resultado').value = "";
-            break;
-        case 'MOD':
-            num = '%';
-            document.getElementById('resultado').value = contenido + num;
-            break;
-            
+            break;       
         default:
             document.getElementById('resultado').value = contenido + num;
             break;
     }
-
-
 }
-
-
-
-
 function finiciar() {
     let aux = document.getElementsByClassName('boton');
     for (let index = 0; index < aux.length; index++) {
         aux[index].addEventListener('click', getValue);
     };
-
 }

58429c1101870a6d316d851c55ebbb410243f982
Samuel <samuel.sanram@educa.jcyl.es>
2024-02-14 21:28:11 +0100

    fin calse

--- REPASO/repaso.js
+++ REPASO/repaso.js
@@ -9,42 +9,51 @@ function iniciar() {
  cuadrados[i].addEventListener('mouseover',mostrartooltip);
  cuadrados[i].addEventListener('mouseout',ocultartooltip);
  cuadrados[i].addEventListener('mousemove',movertooltip);
-    
+cuadrados[i].addEventListener('click',mover);
    }
  }
-function clean(node) {
-        for(var n = 0; n < node.childNodes.length; n ++)
-        {
-          var child = node.childNodes[n];
-          if
-          (
-            child.nodeType === 8 
-            || 
-            (child.nodeType === 3 && !/\S/.test(child.nodeValue))
-          )
-          {
-            node.removeChild(child);
-            n --;
-          }
-          else if(child.nodeType === 1)
-          {
-            clean(child);
-          }}}
 function mostrartooltip(e) {
-    let tool=document.createElement('div');
+   /* let tool=document.createElement('div');
     let aux= document.createTextNode(e.target.getAttribute('data-tooltip'));
     tool.appendChild(aux);
     tool.setAttribute('class','tooltip');
-    e.target.appendChild(tool);
+    e.target.appendChild(tool);*/
 }
 function ocultartooltip(e) {
    /*var tools= document.getElementsByClassName('tooltip');
-   tools[0].remove();*/
+   tools[0].remove();
     var elemento = e.target.firstChild;
-     e.target.removeChild(elemento);
+     e.target.removeChild(elemento);*/
 }
 function movertooltip(e) {
-    var tooltip= e.target.firstChild;
-    tooltip.style.left=e.clientX+30+'px';
-    tooltip.style.top= e.clientY-25+'px';
+   /* var tooltip= e.target.firstChild;
+    tooltip.style.left= e.clientX+30+'px';
+    tooltip.style.top= e.clientY-25+'px';*/
 }
+function mover (e){
+    if (e.target.nextSibling) {
+      // e.target.nextSibling.after(e.target);
+       // var element= e.target;
+       // e.target.previusSibling=post_sibling;
+       var prev_s=e.target.nextSibling;
+     prev_s.insertAdjacentElement('afterend',e.target);
+    }
+}
+function clean(node) {
+    for(var n = 0; n < node.childNodes.length; n ++)
+    {
+      var child = node.childNodes[n];
+      if
+      (
+        child.nodeType === 8 
+        || 
+        (child.nodeType === 3 && !/\S/.test(child.nodeValue))
+      )
+      {
+        node.removeChild(child);
+        n --;
+      }
+      else if(child.nodeType === 1)
+      {
+        clean(child);
+      }}}
